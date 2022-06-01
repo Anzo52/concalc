206e7b54e9ca7d6a5128ee954010a97edec81fe0
Anzo52 <andy@zollnersolutions.com>
2022-06-01 14:35:56 -0600

    untested

--- concalc.py
+++ concalc.py
@@ -13,7 +13,6 @@
 
 
 from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
 from flask_cors import CORS
 import json
 import requests
@@ -23,11 +22,6 @@ import math
 
 
 app = Flask(__name__)
-from flask_autodoc import Autodoc
-
-auto = Autodoc(app)
-
-api = Api(app)
 
 def inches_to_yards(inches):
     return inches / 36
@@ -35,70 +29,104 @@ def inches_to_yards(inches):
 def feet_to_yards(feet):
     return feet / 3
 
-class curbGutter(Resource):
-    def get(self):
-        # Get the base width, toe height, face height, curb width, and back height
-        base_width_inches = request.args.get('base_width_inches')
-        toe_height_inches = request.args.get('toe_height_inches')
-        face_height_inches = request.args.get('face_height_inches')
-        curb_width_inches = request.args.get('curb_width_inches')
-        back_height_inches = request.args.get('back_height_inches')
-        length_feet = request.args.get('length_feet')
 
-        # Calculate the volume of concrete needed to fill the curb gutter
-        base_width_yards = inches_to_yards(base_width_inches)
-        toe_height_yards = inches_to_yards(toe_height_inches)
-        face_height_yards = inches_to_yards(face_height_inches)
-        curb_width_yards = inches_to_yards(curb_width_inches)
-        back_height_yards = inches_to_yards(back_height_inches)
-        length_yards = feet_to_yards(length_feet)
-        vol_cubic_yards = (base_width_yards * toe_height_yards * face_height_yards * curb_width_yards * back_height_yards * length_yards)
+def get_curb_gutter_volume(base_width_inches, toe_height_inches, face_height_inches, curb_width_inches, length_feet):
+    base_width_yards = inches_to_yards(base_width_inches)
+    toe_height_yards = inches_to_yards(toe_height_inches)
+    face_height_yards = inches_to_yards(face_height_inches)
+    curb_width_yards = inches_to_yards(curb_width_inches)
+    length_yards = feet_to_yards(length_feet)
 
-        # Return the volume of concrete needed to fill the curb gutter
-        return jsonify({'volume_cubic_yards': vol_cubic_yards})
+    volume_cubic_yards = (base_width_yards * toe_height_yards * length_yards) + (curb_width_yards * face_height_yards * length_yards)
+    return volume_cubic_yards
 
 
-class sidewalk(Resource):
-    def get(self):
-        # Get the thickness and width of the sidewalk
-        thickness_inches = request.args.get('thickness_inches')
-        width_feet = request.args.get('width_feet')
-        length_feet = request.args.get('length_feet')
+def get_sidewalk_volume(thickness_inches, width_feet, length_feet):
+    thickness_yards = inches_to_yards(thickness_inches)
+    width_yards = feet_to_yards(width_feet)
+    length_yards = feet_to_yards(length_feet)
 
-        # Calculate the volume of concrete needed to fill the sidewalk
-        thickness_yards = inches_to_yards(thickness_inches)
-        width_yards = feet_to_yards(width_feet)
-        length_yards = feet_to_yards(length_feet)
-        vol_cubic_yards = (thickness_yards * width_yards * length_yards)
-
-        # Return the volume of concrete needed to fill the sidewalk
-        return jsonify({'volume_cubic_yards': vol_cubic_yards})
+    volume_cubic_yards = thickness_yards * width_yards * length_yards
+    return volume_cubic_yards
 
 
-class patio(Resource):
-    def get(self):
-        # Get the thickness and width of the patio
-        thickness_inches = request.args.get('thickness_inches')
-        width_feet = request.args.get('width_feet')
-        length_feet = request.args.get('length_feet')
+def get_patio_volume(thickness_inches, width_feet, length_feet):
+    thickness_yards = inches_to_yards(thickness_inches)
+    width_yards = feet_to_yards(width_feet)
+    length_yards = feet_to_yards(length_feet)
 
-        # Calculate the volume of concrete needed to fill the patio
-        thickness_yards = inches_to_yards(thickness_inches)
-        width_yards = feet_to_yards(width_feet)
-        length_yards = feet_to_yards(length_feet)
-        vol_cubic_yards = (thickness_yards * width_yards * length_yards)
-
-        # Return the volume of concrete needed to fill the patio
-        return jsonify({'volume_cubic_yards': vol_cubic_yards})
+    volume_cubic_yards = thickness_yards * width_yards * length_yards
+    return volume_cubic_yards
 
 
-api.add_resource(curbGutter, '/concalc/api/v1/curbgutter/<int:base_width_inches>&<int:toe_height_inches>&<int:face_height_inches>&<curb_width_inches>&<back_height_inches>&<length_feet>')
-api.add_resource(sidewalk, '/concalc/api/v1/sidewalk/<int:thickness_inches>&<width_feet>&<length_feet>')
-api.add_resource(patio, '/concalc/api/v1/patio/<int:thickness_inches>&<width_feet>&<length_feet>')
+@app.route('/concalc/test/curbgutter', methods=['GET'])
+def test_curb_gutter():
+    base_width_inches = request.args.get('base_width_inches', type=int)
+    toe_height_inches = request.args.get('toe_height_inches', type=int)
+    face_height_inches = request.args.get('face_height_inches', type=int)
+    curb_width_inches = request.args.get('curb_width_inches', type=int)
+    length_feet = request.args.get('length_feet', type=int)
+
+    volume_cubic_yards = get_curb_gutter_volume(base_width_inches, toe_height_inches, face_height_inches, curb_width_inches, length_feet)
+    return jsonify(volume_cubic_yards=volume_cubic_yards)
+
+
+@app.route('/concalc/test/sidewalk', methods=['GET'])
+def test_sidewalk():
+    thickness_inches = request.args.get('thickness_inches', type=int)
+    width_feet = request.args.get('width_feet', type=int)
+    length_feet = request.args.get('length_feet', type=int)
+
+    volume_cubic_yards = get_sidewalk_volume(thickness_inches, width_feet, length_feet)
+    return jsonify(volume_cubic_yards=volume_cubic_yards)
+
+
+@app.route('/concalc/test/patio', methods=['GET'])
+def test_patio():
+    thickness_inches = request.args.get('thickness_inches', type=int)
+    width_feet = request.args.get('width_feet', type=int)
+    length_feet = request.args.get('length_feet', type=int)
+
+    volume_cubic_yards = get_patio_volume(thickness_inches, width_feet, length_feet)
+    return jsonify(volume_cubic_yards=volume_cubic_yards)
+
+
+@app.route('/concalc/api/v1/curbgutter', methods=['GET'])
+def curb_gutter():
+    base_width_inches = request.args.get('base_width_inches', type=int)
+    toe_height_inches = request.args.get('toe_height_inches', type=int)
+    face_height_inches = request.args.get('face_height_inches', type=int)
+    curb_width_inches = request.args.get('curb_width_inches', type=int)
+    length_feet = request.args.get('length_feet', type=int)
+
+    volume_cubic_yards = get_curb_gutter_volume(base_width_inches, toe_height_inches, face_height_inches, curb_width_inches, length_feet)
+    return jsonify(volume_cubic_yards=volume_cubic_yards)
+
+
+@app.route('/concalc/api/v1/sidewalk', methods=['GET'])
+def sidewalk():
+    thickness_inches = request.args.get('thickness_inches', type=int)
+    width_feet = request.args.get('width_feet', type=int)
+    length_feet = request.args.get('length_feet', type=int)
+
+    volume_cubic_yards = get_sidewalk_volume(thickness_inches, width_feet, length_feet)
+    return jsonify(volume_cubic_yards=volume_cubic_yards)
+
+
+@app.route('/concalc/api/v1/patio', methods=['GET'])
+def patio():
+    thickness_inches = request.args.get('thickness_inches', type=int)
+    width_feet = request.args.get('width_feet', type=int)
+    length_feet = request.args.get('length_feet', type=int)
+
+    volume_cubic_yards = get_patio_volume(thickness_inches, width_feet, length_feet)
+    return jsonify(volume_cubic_yards=volume_cubic_yards)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(host='
 
 
-# End of concalc.py
+# curl -X GET "http://localhost:5000/concalc/api/v1/curbgutter?base_width_inches=12&toe_height_inches=12&face_height_inches=12&curb_width_inches=12&length_feet=12"
+# curl -X GET "http://localhost:5000/concalc/api/v1/sidewalk?thickness_inches=12&width_feet=12&length_feet=12"
